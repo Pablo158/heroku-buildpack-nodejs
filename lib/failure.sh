59143608767442ea13619c1248b1b96f8cac0803
Danielle Adams <danielle.adams@heroku.com>
2020-09-23 17:09:35 -0400

    vendor binaries

--- lib/failure.sh
+++ lib/failure.sh
@@ -666,9 +666,9 @@ warning() {
 warn() {
   local tip=${1:-}
   local url=${2:-https://devcenter.heroku.com/articles/nodejs-support}
-  echo " !     $tip" || true
-  echo "       $url" || true
-  echo ""
+  echo -e " !     $tip" || true
+  echo -e "       $url" || true
+  echo -e ""
 }
 
 warn_aws_proxy() {
@@ -697,12 +697,8 @@ warn_vulnerable_node() {
   node_version="$1"
   security_release="$(security $BP_DIR/inventory/node.toml $node_version)"
 
-  echo "checking for security release..."
-  echo "node version: $node_version"
-  echo "security release: $security_release"
-
   if [[ "$security_release" != "" ]]; then
-    warn "This version of Node has a known vulnerability. Update to the security release, $security_release to patch."
+    warn "\nThis version of Node has a known vulnerability. Update to the security release, $security_release to patch."
     mcount 'warnings.node.vulnerable'
   fi
 }
